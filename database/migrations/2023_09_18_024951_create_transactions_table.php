62d615c2f3d6ce860d6784fbec570857439ad62f
Tarikulislamakash <tarikulislamakash@gmail.com>
2023-09-18 09:10:05 +0600

    initial commit

---
+++ database/migrations/2023_09_18_024951_create_transactions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->enum('transaction_type', ['1', '2'])->comment('1 => individual, 2 => business');
+            $table->double('amount', 8, 2)->nullable();
+            $table->decimal('fee', 8, 2)->nullable();
+            $table->date('date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transactions');
+    }
+};
